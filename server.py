a1b1dfe2e3b2a4a4e3a7c59f48b643f91bbebbd2
jenchen-dev <jeanchen0613@gmail.com>
2024-06-26 23:08:06 +0800

    replace with profolio files

--- server.py
+++ server.py
@@ -2,16 +2,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/<username>/<int:post_id>')
-def hello_world(username=None, post_id=None):
-    return render_template('./index.html', name=username, post_id=post_id)
+@app.route('/')
+def my_home():
+    return render_template('index.html')
 
 
-@app.route('/blog')
-def blog():
-    return 'This page is blog page'
-
-
-@app.route('/blog/2024/dogs')
-def blog_post():
-    return 'This is my dog'
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
